c68ab0e42d26a4899e19c8cd5c78761377be8a9d
MadhavDesai207 <mddesai207@gmail.com>
2025-03-08 12:59:55 +0530

    frontend added.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import NFTFractionalization from "./components/NFTFractionalization";
+
+function App() {
+  return (
+    <div className="App">
+      <NFTFractionalization />
+    </div>
+  );
+}
+
+export default App;
